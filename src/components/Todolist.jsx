408c2022d52bc50f658abcb79d7a238c704fc41f
Arthur Y <selimkusainov@MacBook-Pro-Selim.local>
2023-03-18 15:29:49 +0300

    addTouchFunction

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -16,13 +16,13 @@ const TodoList = () => {
 
   let timeout
 
-  function down () {
+  function down() {
     timeout = setTimeout(() => {
       setShowDel(!showDel)
     }, 600)
   }
 
-  function up () {
+  function up() {
     clearTimeout(timeout)
   }
 
@@ -30,16 +30,23 @@ const TodoList = () => {
     <div className="todos" >
       {todos.map((todo) => (
         <div key={todo.id} className="todo">
-          <label className="checkcase" style={{backgroundColor: todo.completed ? "#FFD52E" : null, border: todo.completed ? "1px solid #FFD52E" : "1px solid #b7b7b7"}}>
-            {todo.completed ? <div className="symbol"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36"><path fill="#31373D" d="M34.459 1.375c-1.391-.902-3.248-.506-4.149.884L13.5 28.17l-8.198-7.58c-1.217-1.125-3.114-1.051-4.239.166-1.125 1.216-1.051 3.115.166 4.239l10.764 9.952s.309.266.452.359c.504.328 1.07.484 1.63.484.982 0 1.945-.482 2.52-1.368L35.343 5.524c.902-1.39.506-3.248-.884-4.149z"/></svg></div> : null} 
-          <input
-            className="checkbox"
-            type="checkbox"
-            checked={todo.completed}
-            onChange={() => dispatch(toggleTodo(todo.id))}
-          />
+          <label className="checkcase" style={{ backgroundColor: todo.completed ? "#FFD52E" : null, border: todo.completed ? "1px solid #FFD52E" : "1px solid #b7b7b7" }}>
+            {todo.completed ? <div className="symbol"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36"><path fill="#31373D" d="M34.459 1.375c-1.391-.902-3.248-.506-4.149.884L13.5 28.17l-8.198-7.58c-1.217-1.125-3.114-1.051-4.239.166-1.125 1.216-1.051 3.115.166 4.239l10.764 9.952s.309.266.452.359c.504.328 1.07.484 1.63.484.982 0 1.945-.482 2.52-1.368L35.343 5.524c.902-1.39.506-3.248-.884-4.149z" /></svg></div> : null}
+            <input
+              className="checkbox"
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => dispatch(toggleTodo(todo.id))}
+            />
           </label>
-          <div className="text" onDoubleClick={() => setShowDel(!showDel)} onMouseDown={() => down()} onMouseUp={() => up()} style={{textDecoration: todo.completed ? "line-through 2px #FFD52E" : null}}>{todo.title}</div>
+          <div className="text"
+            // onDoubleClick={() => setShowDel(!showDel)}
+            onTouchStart={() => down()}
+            onTouchEnd={() => up()}
+            onMouseDown={() => down()}
+            onMouseUp={() => up()}
+            style={{ textDecoration: todo.completed ? "line-through 2px #FFD52E" : null }}>{todo.title}
+          </div>
           {showDel ? <div className="delete" onClick={() => dispatch(removeTodo(todo.id))}><div className="minus"></div></div> : null}
         </div>
       ))}
