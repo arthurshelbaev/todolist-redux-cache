17d68fb8eded2ac50a34db728768ea23a3e1958e
Arthur Y <konodiobrandoda@mail.ru>
2023-12-25 16:05:00 +0500

    Update Newtodo.jsx

--- src/components/Newtodo.jsx
+++ src/components/Newtodo.jsx
@@ -16,9 +16,6 @@ const NewTodo = () => {
   return (
     <form className="form" onSubmit={handleSubmit}>
       <input className="input" type="text" name="title" placeholder="Нажмите Enter, чтобы добавить задачу." />
-      <input className="input" type="text" name="title" placeholder="Нажмите Enter, чтобы добавить задачу." />
-      <input className="input" type="date" name="title" placeholder="Нажмите Enter, чтобы добавить задачу." />
-      <input type="text" name="title" placeholder="Нажмите Enter, чтобы добавить задачу." />
     </form>
   );
 };
